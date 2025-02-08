9442f0ba0f546e4436de94c2b7d230ee44d2778f
Holly Cheng <github@howcheng.com>
2025-02-08 14:24:03 -0800

    ITiebreakerColumnsRequestCreator moved to new file and removed unused private vars

---
+++ src/Services/Interfaces/ITiebreakerColumnsRequestCreator.cs
@@ -0,0 +1,7 @@
+﻿namespace PantherShootoutScoreSheetGenerator.Services
+{
+	public interface ITiebreakerColumnsRequestCreator
+	{
+		PoolPlayInfo CreateTiebreakerRequests(PoolPlayInfo info, IEnumerable<Team> poolTeams, int startRowIndex);
+	}
+}
--- src/Services/RequestCreators/TiebreakerColumnsRequestCreator.cs
+++ src/Services/RequestCreators/TiebreakerColumnsRequestCreator.cs
@@ -4,27 +4,18 @@ using StandingsGoogleSheetsHelper;
 
 namespace PantherShootoutScoreSheetGenerator.Services
 {
-	public interface ITiebreakerColumnsRequestCreator
-	{
-		PoolPlayInfo CreateTiebreakerRequests(PoolPlayInfo info, IEnumerable<Team> poolTeams, int startRowIndex);
-	}
-
 	/// <summary>
-	/// Creates the requests used to build the tiebreaker columns; these must be done after all the standings tables have been created because they may need
-	/// to span multiple pools (in the case of a 10-team division)
+	/// Creates the requests used to build the tiebreaker columns in the standings table; these must be done after all the standings tables have been created 
+	/// because they may need to span multiple pools (in the case of a 10-team division)
 	/// </summary>
 	public class TiebreakerColumnsRequestCreator : ITiebreakerColumnsRequestCreator
 	{
 		private readonly DivisionSheetConfig _config;
-		private readonly PsoFormulaGenerator _formulaGenerator;
-		private readonly PsoDivisionSheetHelper _helper;
 		private readonly IStandingsRequestCreatorFactory _requestCreatorFactory;
 
-		public TiebreakerColumnsRequestCreator(DivisionSheetConfig config, FormulaGenerator formGen, IStandingsRequestCreatorFactory requestCreatorFactory)
+		public TiebreakerColumnsRequestCreator(DivisionSheetConfig config, IStandingsRequestCreatorFactory requestCreatorFactory)
 		{
 			_config = config;
-			_formulaGenerator = (PsoFormulaGenerator)formGen;
-			_helper = (PsoDivisionSheetHelper)formGen.SheetHelper;
 			_requestCreatorFactory = requestCreatorFactory;
 		}
 
@@ -48,7 +39,7 @@ namespace PantherShootoutScoreSheetGenerator.Services
 					FirstTeamsSheetCell = firstTeamSheetCell,
 					RowCount = _config.TeamsPerPool,
 					StandingsStartAndEndRowNums = info.StandingsStartAndEndRowNums,
-					ScoreEntryStartAndEndRowNums = info.ScoreEntryStartAndEndRowNums,
+					ScoreEntryStartAndEndRowNums = new(info.FirstScoreEntryRowNum, info.LastScoreEntryRowNum),
 				};
 
 				IStandingsRequestCreator requestCreator = _requestCreatorFactory.GetRequestCreator(hdr);
